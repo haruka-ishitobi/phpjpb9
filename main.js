cbcdf11568efc926787c02069acfede402fce41d
haruka-ishitobi <127273500+haruka-ishitobi@users.noreply.github.com>
2023-05-30 21:04:25 +0900

    課題提出ファイル９

---
+++ main.js
@@ -0,0 +1,16 @@
+
+click.addEventListener('click',function(){
+    let left = document.querySelector('.left');
+    left.classList.add('.left.action');
+
+
+
+},false);
+
+
+
+click.addEventListener('click',function(){
+    let right = document.querySelector('.right');
+    right.classList.add('.right.action');
+
+},false);
